1d6a834045dbec2a2fc9ef69aef11ecc7dde1c7f
Alex Fomin <ShurickFomin@gmail.com>
2015-08-27 17:32:48 +0300

    Change langVersion to C#6

--- src/Tp.Core.Functional/DictionaryExtensions.cs
+++ src/Tp.Core.Functional/DictionaryExtensions.cs
@@ -8,9 +8,7 @@ namespace System.Collections.Generic
 	{
 		public static Maybe<TVal> GetValue<TKey, TVal>(this IDictionary<TKey, TVal> d, TKey k)
 		{
-			if (k == null)
-				return Maybe.Nothing;
-			return Maybe.FromTryOut<TKey, TVal>(d.TryGetValue, k);
+			return k == null ? Maybe.Nothing : Maybe.FromTryOut<TKey, TVal>(d.TryGetValue, k);
 		}
 	}
 }
