7e48e1c018c4d432518eae0cf0858c580f405610
Sergey Mamulchenko <mamulchenko@targetprocess.com>
2016-06-28 19:49:42 +0300

    api key removed

--- deploy_dotnetcore.cmd
+++ deploy_dotnetcore.cmd
@@ -6,6 +6,6 @@ dotnet build -c Release
 dotnet pack --no-build -c Release -o packages
 cd ../..
 
-nuget\nuget.exe push src\Tp.Core.Functional\packages\Tp.Core.Functional.%VERSION%.nupkg -ApiKey e97347be-0832-4e37-a2de-7b7128b50d88 -Source "https://api.nuget.org/v3/index.json
+nuget\nuget.exe push src\Tp.Core.Functional\packages\Tp.Core.Functional.%VERSION%.nupkg -Source "https://api.nuget.org/v3/index.json
 
 pause
