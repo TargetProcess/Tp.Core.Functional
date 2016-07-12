d486d7d98304ebef47bf58616c11159ed8e31c9d
zubritsky <zubritsky@targetprocess.com>
2016-07-12 19:19:08 +0300

    Add infrastructure for performance optimization

---
+++ src/Tp.Core.Functional.Benchmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Tp.Core.Functional.Benchmarks
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+
+		private static Summary RunBenchmark<T>()
+		{
+			return BenchmarkRunner.Run<T>(
+				ManualConfig
+					.Create(DefaultConfig.Instance)
+					.With(new MemoryDiagnoser())
+					.With(MarkdownExporter.GitHub));
+		}
+	}
+}
