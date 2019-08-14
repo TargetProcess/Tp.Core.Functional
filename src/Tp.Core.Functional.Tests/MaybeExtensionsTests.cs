090ba9742f33cf14137d05664cbd24e4fde88cfb
Petr Koutny <petr.koutny@solarwinds.com>
2019-08-14 19:32:43 +0200

    Added extension method ToMaybe<string>

---
+++ src/Tp.Core.Functional.Tests/MaybeExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿//
+// Copyright (c) 2005-2011 TargetProcess. All rights reserved.
+// TargetProcess proprietary/confidential. Use is subject to license terms. Redistribution of this file is strictly forbidden.
+//
+
+using NUnit.Framework;
+
+namespace Tp.Core.Functional.Tests
+{
+	[TestFixture]
+	public class MaybeExtensionsTests : TestBase
+	{
+		[Test]
+		public void StringToMaybeTest()
+		{
+			string nullString = null;
+			string emptyString = string.Empty;
+			string valueString = "abc";
+
+			Assert.IsFalse(nullString.ToMaybe(treatEmptyAsNull: true).HasValue);
+			Assert.IsFalse(nullString.ToMaybe(treatEmptyAsNull: false).HasValue);
+
+			Assert.IsFalse(emptyString.ToMaybe(treatEmptyAsNull: true).HasValue);
+			Assert.IsTrue(emptyString.ToMaybe(treatEmptyAsNull: false).HasValue);
+
+			Assert.IsTrue(valueString.ToMaybe(treatEmptyAsNull: true).HasValue);
+			Assert.IsTrue(valueString.ToMaybe(treatEmptyAsNull: false).HasValue);
+		}
+	}
+}
---
+++ src/Tp.Core.Functional/MaybeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Tp.Core
+{
+	public static class MaybeExtensions
+	{
+		public static Maybe<string> ToMaybe(this string str, bool treatEmptyAsNull = true)
+		{
+			if (str == null || treatEmptyAsNull && string.IsNullOrEmpty(str))
+			{
+				return Maybe<string>.Nothing;
+			}
+
+			return Maybe.Just(str);
+		}
+	}
+}
